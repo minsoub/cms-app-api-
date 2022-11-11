461c2568f095e62865374e1e2081dc73b6e42e6a
rihongo-at-807380035085 <3zaHkd3zJ87T1icEEOSTNLAHlAdGniO9sZT2LJlmeo4=>
2022-11-11 22:35:52 +0900

    - QueryParam 추가

--- src/main/kotlin/com/bithumbsystems/cms/api/util/GsonUtil.kt
+++ src/main/kotlin/com/bithumbsystems/cms/api/util/GsonUtil.kt
@@ -1,10 +1,12 @@
 package com.bithumbsystems.cms.api.util
 
+import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
 object GsonUtil {
     val gson: Gson = GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
         .disableHtmlEscaping()
         .setPrettyPrinting()
         .create()
