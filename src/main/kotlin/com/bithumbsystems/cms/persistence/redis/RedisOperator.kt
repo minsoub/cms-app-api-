667045ea36bcbc384bf9b425a5e3d1596c44b671
sm <seoungmin104@bithumbsystems.com>
2022-12-22 10:20:00 +0900

    Fix: redis objectMapper 주입 변경 get 할 시 typeReference로 변경

--- src/main/kotlin/com/bithumbsystems/cms/persistence/redis/RedisOperator.kt
+++ src/main/kotlin/com/bithumbsystems/cms/persistence/redis/RedisOperator.kt
@@ -6,23 +6,18 @@ import com.bithumbsystems.cms.persistence.redis.model.RedisNoticeCategory
 import com.bithumbsystems.cms.persistence.redis.model.RedisNoticeFix
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import kotlinx.coroutines.reactor.awaitSingle
 import org.redisson.api.RBucketReactive
 import org.redisson.api.RedissonReactiveClient
 import org.redisson.codec.TypedJsonJacksonCodec
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 
 @Service
 class RedisOperator(
     private val redissonReactiveClient: RedissonReactiveClient,
+    private val objectMapper: ObjectMapper
 ) {
     suspend fun getTopNotice(): List<RedisNoticeFix> {
-        val objectMapper = ObjectMapper()
-        objectMapper.registerModule(JavaTimeModule())
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         val typeReference = object : TypeReference<List<RedisNoticeFix>>() {}
 
         return redissonReactiveClient
@@ -32,19 +27,15 @@ class RedisOperator(
     }
 
     suspend fun setTopNotice(noticeList: List<RedisNoticeFix>): Void {
-
-        val objectMapper = ObjectMapper()
-        objectMapper.registerModule(JavaTimeModule())
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        val typeReference = object : TypeReference<List<RedisNoticeFix>>() {}
 
         val bucket: RBucketReactive<List<RedisNoticeFix>> = redissonReactiveClient
-            .getBucket(RedisKey.REDIS_NOTICE_FIX_KEY, TypedJsonJacksonCodec(List::class.java, LocalDateTime::class.java, objectMapper))
+            .getBucket(RedisKey.REDIS_NOTICE_FIX_KEY, TypedJsonJacksonCodec(typeReference, objectMapper))
 
         return bucket.set(noticeList).awaitSingle()
     }
 
     suspend fun getNoticeCategory(): List<NoticeCategoryResponse> {
-        val objectMapper = ObjectMapper()
         val typeReference = object : TypeReference<List<NoticeCategoryResponse>>() {}
 
         return redissonReactiveClient
@@ -54,8 +45,10 @@ class RedisOperator(
     }
 
     suspend fun setNoticeCategory(categoryList: List<RedisNoticeCategory>): Void {
+        val typeReference = object : TypeReference<List<NoticeCategoryResponse>>() {}
+
         val bucket: RBucketReactive<List<RedisNoticeCategory>> = redissonReactiveClient
-            .getBucket(RedisKey.REDIS_NOTICE_CATEGORY_KEY, TypedJsonJacksonCodec(List::class.java))
+            .getBucket(RedisKey.REDIS_NOTICE_CATEGORY_KEY, TypedJsonJacksonCodec(typeReference, objectMapper))
 
         return bucket.set(categoryList).awaitSingle()
     }
